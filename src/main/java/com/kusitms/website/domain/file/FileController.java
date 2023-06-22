8cc70c0a88ce5e22bf73c110aae47148a56088ae
Jeongminyooa <jmjmjmz732002@naver.com>
2023-06-23 01:20:51 +0900

    fix : file 컨트롤러 스웨거 출력 안함

--- src/main/java/com/kusitms/website/domain/file/FileController.java
+++ src/main/java/com/kusitms/website/domain/file/FileController.java
@@ -1,10 +1,7 @@
 package com.kusitms.website.domain.file;
 
 import com.kusitms.website.global.common.BaseResponse;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.Hidden;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,18 +12,14 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
+@Hidden
 @RestController
 @RequestMapping("/api/files")
 @RequiredArgsConstructor
-@Tag(name = "File", description = "파일 업로드 API Document")
 public class FileController {
     private final S3Service s3Service;
 
     @PostMapping()
-    @Operation(summary = "파일 업로드", description = "Multipartfile을 업로드 합니다.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "업로드 성공")
-    })
     public ResponseEntity<BaseResponse> postFile(@RequestPart("file_request") MultipartFile fileRequest) throws IOException {
         return ResponseEntity.ok(new BaseResponse(s3Service.uploadFile(fileRequest, "upload")));
     }
