ae8c111de1fd21defadc7c67c9d7cdc4faa01425
mino <dlalsgh6772@gmail.com>
2025-12-21 13:41:26 +0900

    feat : 모집 이메일 전송 이메일 저장 API 구현

--- src/main/java/com/kusitms/website/domain/blog/service/BlogService.java
+++ src/main/java/com/kusitms/website/domain/blog/service/BlogService.java
@@ -1,18 +1,14 @@
 package com.kusitms.website.domain.blog.service;
 
 import com.kusitms.website.domain.blog.dto.response.BlogResponse;
-import com.kusitms.website.domain.blog.entity.BlogPost;
 import com.kusitms.website.domain.blog.entity.Category;
 import com.kusitms.website.domain.blog.entity.Position;
-import com.kusitms.website.domain.blog.repository.BlogPostQueryRepository;
 import com.kusitms.website.domain.blog.repository.BlogPostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
--- src/main/java/com/kusitms/website/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/kusitms/website/global/exception/GlobalExceptionHandler.java
@@ -1,12 +1,26 @@
 package com.kusitms.website.global.exception;
 
+import com.kusitms.website.global.common.BaseException;
 import com.kusitms.website.global.common.BaseExceptionStatus;
 import com.kusitms.website.global.common.BaseResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<BaseResponse<BaseException>> handleIllegalArgument(
+            IllegalArgumentException ex
+    ) {
+        return ResponseEntity
+                .badRequest()
+                .body(BaseResponse.error(
+                        BaseExceptionStatus.BAD_REQUEST,
+                        ex.getMessage()
+                ));
+    }
 
     @ExceptionHandler(RuntimeException.class)
     public BaseResponse handleException(RuntimeException ex) {
