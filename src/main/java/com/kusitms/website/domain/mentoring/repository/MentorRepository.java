8d56f2a90dbba6fb9b55bb3e9fad74f64adda4ee
Hoyeon Lee <89958157+howooyeon@users.noreply.github.com>
2026-06-29 00:06:22 +0900

    fix: 멘토 조회 N+1 개선 시 count 쿼리 오류 수정

--- src/main/java/com/kusitms/website/domain/mentoring/repository/MentorRepository.java
+++ src/main/java/com/kusitms/website/domain/mentoring/repository/MentorRepository.java
@@ -4,10 +4,9 @@ import com.kusitms.website.domain.mentoring.entity.Mentor;
 import com.kusitms.website.domain.mentoring.entity.MentoringCategory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -16,9 +15,9 @@ public interface MentorRepository extends JpaRepository<Mentor, Long> {
     @Query("SELECT m FROM Mentor m JOIN FETCH m.member WHERE m.active = true ORDER BY FUNCTION('RANDOM')")
     List<Mentor> findRandomActiveMentors(Pageable pageable);
 
-    @Query("SELECT m FROM Mentor m JOIN FETCH m.member WHERE m.active = true ORDER BY m.createdAt DESC")
+    @EntityGraph(attributePaths = "member")
     Page<Mentor> findByActiveTrueOrderByCreatedAtDesc(Pageable pageable);
 
-    @Query("SELECT m FROM Mentor m JOIN FETCH m.member WHERE m.active = true AND m.category = :category ORDER BY m.createdAt DESC")
-    Page<Mentor> findByActiveTrueAndCategoryOrderByCreatedAtDesc(@Param("category") MentoringCategory category, Pageable pageable);
+    @EntityGraph(attributePaths = "member")
+    Page<Mentor> findByActiveTrueAndCategoryOrderByCreatedAtDesc(MentoringCategory category, Pageable pageable);
 }
